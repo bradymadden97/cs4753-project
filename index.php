c0fdc3ab64a0f3bcf25d52eda5431bfe99cc0146
Robert Hails <rlh2ph@virginia.edu>
2017-09-06 15:12:55 -0400

    php practice

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
 
+echo "<h2>CS-4753 Project</h2>";
 echo "Hello world! this is a test.<br>";
-echo "Bobby is in"
+echo "Bobby is in<br><br>";
 
+$best_idea = "bottled air";
+echo "Best idea for the business so far: $best_idea<br>";
 
 ?>
