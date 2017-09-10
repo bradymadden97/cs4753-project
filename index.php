a48f8b45e8f2d663625c1ba2c91fa1395f0478b0
Brady Madden <bradymadden97@gmail.com>
2017-09-10 12:38:35 -0400

    init bootstrap

--- index.php
+++ index.php
@@ -1,10 +1,48 @@
-<?php
+<html>
+<head>
+	<title>
+		Homepage Title
+	</title>
 
-echo "<h2>CS-4753 Project</h2>";
-echo "Hello world! this is a test.<br>";
-echo "Bobby is in<br><br>";
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+	<link rel="stylesheet" href="./css/homepage.css">
+</head>
+<body>
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		<a class="navbar-brand" href="">Title</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-dropdown" aria-controls="#navbar-dropdown" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbar-dropdown">
+			<div class="nav-full">
+				<ul class="navbar-nav">
+					<li class="nav-item">
+						<a href="./about">About</a>
+					</li>
+				</ul>
+			</div>
+			<span>
+				<ul class="navbar-nav"
+					<li class="nav-item">
+						<button class="btn btn-success btn-margin-5w" type="button" href="./register">Sign Up</button>
+					</li>
+					<li class="nav-item">
+						<button class="btn btn-outline-success btn-margin-5w" type="button" ="./login">Login</button>
+					</li>
+				</ul>
+			</span>
+		</div>
+	</nav>
+	<div class="container-fluid">
+	
+	</div>
 
-$best_idea = "bottled air";
-echo "Best idea for the business so far: $best_idea<br>";
 
-?>
+	<!-- JQuery, Popper, and Bootstrap JS should remain at bottom of Body -->
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+</body>
+</html>
