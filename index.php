0e428e144a957847bf4552b4a5866a4830666423
Brady Madden <bradymadden97@gmail.com>
2017-09-10 15:53:36 -0400

    updated homepage

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <title>Creative - Start Bootstrap Theme</title>
+    <title>Company Name - Slogan</title>
 
     <!-- Bootstrap core CSS -->
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -20,6 +20,9 @@
 
     <!-- Custom styles for this template -->
     <link href="css/creative.min.css" rel="stylesheet">
+	
+	<!--Our own css -->
+	<link href="css/homepage.css" rel="stylesheet">
 
   </head>
 
@@ -28,24 +31,24 @@
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
       <div class="container">
-        <a class="navbar-brand js-scroll-trigger" href="#page-top">Start Bootstrap</a>
+        <a class="navbar-brand js-scroll-trigger" href="#page-top">Company Name</a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
           <ul class="navbar-nav ml-auto">
             <li class="nav-item">
-              <a class="nav-link js-scroll-trigger" href="#about">About</a>
+              <a class="nav-link js-scroll-trigger nav-margin-10px" href="./about">About</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link js-scroll-trigger" href="#services">Services</a>
+              <a class="nav-link js-scroll-trigger nav-margin-10px" href="./shop">Shop</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a>
+              <a class="btn-solid-color nav-link js-scroll-trigger" id="signup-btn" href="./register"><span class="nav-margin-10px btn-padding-20 btn-lookalike">Sign up</span></a>
             </li>
             <li class="nav-item">
-              <a class="nav-link js-scroll-trigger" href="#contact">Contact</a>
-            </li>
+              <a class="btn-outline nav-link js-scroll-trigger nav-margin-10px btn-padding-20 btn-lookalike" href="./login">Login</a>
+            </li>			
           </ul>
         </div>
       </div>
@@ -56,8 +59,8 @@
         <div class="header-content-inner">
           <h1 id="homeHeading">It's air, but in a can...<br>...it's canned air</h1>
           <hr>
-          <p>Start Bootstrap can help you build better websites using the Bootstrap CSS framework! Just download your template and start going, no strings attached!</p>
-          <a class="btn btn-primary btn-xl js-scroll-trigger" href="#about">Shop Now</a>
+		  <p>Here we might put our company slogan? Or what we do/ sell.</p><br><br>
+		  <a class="btn btn-primary btn-xxl js-scroll-trigger" href="./shop">Shop Now</a>
         </div>
       </div>
     </header>
