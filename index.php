81605a03be34345f08be2bd1957c0619e1d61e4a
Brady Madden <bradymadden97@gmail.com>
2017-09-13 13:33:01 -0400

    updating more of the homepage

--- index.php
+++ index.php
@@ -98,24 +98,11 @@
       
     </header>
 
-    <section class="bg-primary" id="about">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-8 mx-auto text-center">
-            <h2 class="section-heading text-white">We've got what you need!</h2>
-            <hr class="light">
-            <p class="text-faded">Start Bootstrap has everything you need to get your new website up and running in no time! All of the templates and themes on Start Bootstrap are open source, free to download, and easy to use. No strings attached!</p>
-            <a class="btn btn-default btn-xl js-scroll-trigger" href="#services">Get Started!</a>
-          </div>
-        </div>
-      </div>
-    </section>
-
     <section id="services">
       <div class="container">
         <div class="row">
           <div class="col-lg-12 text-center">
-            <h2 class="section-heading">At Your Service</h2>
+            <h2 class="section-heading">Live Clean, Breathe Easy</h2>
             <hr class="primary">
           </div>
         </div>
@@ -124,30 +111,30 @@
         <div class="row">
           <div class="col-lg-3 col-md-6 text-center">
             <div class="service-box">
-              <i class="fa fa-4x fa-diamond text-primary sr-icons"></i>
-              <h3>Sturdy Templates</h3>
-              <p class="text-muted">Our templates are updated regularly so they don't break.</p>
+              <i class="fa fa-4x fa-globe text-primary sr-icons"></i>
+              <h3>Globally Sourced</h3>
+              <p class="text-muted">Our air comes from the cleanest sources around the globe.</p>
             </div>
           </div>
           <div class="col-lg-3 col-md-6 text-center">
             <div class="service-box">
-              <i class="fa fa-4x fa-paper-plane text-primary sr-icons"></i>
+              <i class="fa fa-4x fa-send text-primary sr-icons"></i>
               <h3>Ready to Ship</h3>
-              <p class="text-muted">You can use this theme as is, or you can make changes!</p>
+              <p class="text-muted">We ship worldwide, so you can breathe cleaner everywhere.</p>
             </div>
           </div>
           <div class="col-lg-3 col-md-6 text-center">
             <div class="service-box">
-              <i class="fa fa-4x fa-newspaper-o text-primary sr-icons"></i>
-              <h3>Up to Date</h3>
-              <p class="text-muted">We update dependencies to keep things fresh.</p>
+              <i class="fa fa-4x fa-child text-primary sr-icons"></i>
+              <h3>Health Benefits</h3>
+              <p class="text-muted">Our air has been proven to provide health benefits to users in poor-air regions.</p>
             </div>
           </div>
           <div class="col-lg-3 col-md-6 text-center">
             <div class="service-box">
               <i class="fa fa-4x fa-heart text-primary sr-icons"></i>
               <h3>Made with Love</h3>
-              <p class="text-muted">You have to make your websites with love these days!</p>
+              <p class="text-muted">Each of our cannisters is packed by hand to ensure the highest quality.</p>
             </div>
           </div>
         </div>
@@ -251,10 +238,10 @@
       </div>
     </section>
 
-    <div class="call-to-action bg-dark">
+    <div class="call-to-action bg-brand">
       <div class="container text-center">
-        <h2>Free Download at Start Bootstrap!</h2>
-        <a class="btn btn-default btn-xl sr-button" href="http://startbootstrap.com/template-overviews/creative/">Download Now!</a>
+        <h2>Check out all our locations around the world!</h2>
+        <a class="btn btn-default btn-xl sr-button" href="/shop">Shop Now</a>
       </div>
     </div>
 
@@ -264,18 +251,18 @@
           <div class="col-lg-8 mx-auto text-center">
             <h2 class="section-heading">Let's Get In Touch!</h2>
             <hr class="primary">
-            <p>Ready to start your next project with us? That's great! Give us a call or send us an email and we will get back to you as soon as possible!</p>
+			<p>Have questions about our product? Want to tell us how we're doing? That's great! Give us a call or send us an email and we will get back to you as soon as possible.</p>
           </div>
         </div>
         <div class="row">
           <div class="col-lg-4 ml-auto text-center">
-            <i class="fa fa-phone fa-3x sr-contact"></i>
-            <p>123-456-6789</p>
+            <i class="fa fa-phone fa-2x sr-contact"></i>
+            <p class="iconspace" >434-321-4321</p>
           </div>
           <div class="col-lg-4 mr-auto text-center">
-            <i class="fa fa-envelope-o fa-3x sr-contact"></i>
-            <p>
-              <a href="mailto:your-email@your-domain.com">feedback@startbootstrap.com</a>
+            <i class="fa fa-envelope-o fa-2x sr-contact"></i>
+            <p class="iconspace" >
+              <a href="mailto:your-email@your-domain.com">contactus@cannedair.com</a>
             </p>
           </div>
         </div>
