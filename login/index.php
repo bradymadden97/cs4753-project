a917cb1f4297077cb4ddce9d9feb7beb59572f82
Brady Madden <bradymadden97@gmail.com>
2017-09-17 23:19:32 -0400

    header imgs, nav-dark

--- login/index.php
+++ login/index.php
@@ -61,5 +61,9 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js"></script>
+	
+	<!-- Custom scripts for this template -->
+    <script src="../js/creative.min.js"></script>
+	
   </body>
 </html>
