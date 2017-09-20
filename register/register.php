5d7cce649fe4fd16f738c6e5201a44542af6466c
Brady Madden <bradymadden97@gmail.com>
2017-09-19 21:36:15 -0400

    error handling on register.php

--- register/register.php
+++ register/register.php
@@ -1,84 +1,106 @@
 <?php
 
 require_once("../config/config.php");
-	
-try {	
-	$first_name;
-	$last_name;
-	$email;
-	$pass;
-	$pass_retype;
-	
+
+
+function validate_first_name(){
 	if(isset($_POST['first_name'])){
 		if(ctype_alpha($_POST['first_name'])){
-			$first_name = $_POST['first_name'];
+			return $_POST['first_name'];
 		}else{
-			//Throw non-alphabetic name error
+			header("Location:index.php?err=fn&type=nonalpha");
+			die();
 		}
 	}else{
-		//Throw missing error
+		header("Location:index.php?err=fn&type=missing");
+		die();
 	}
-	
+}
+
+function validate_last_name(){
 	if(isset($_POST['last_name'])){
 		if(ctype_alpha($_POST['last_name'])){
-			$last_name = $_POST['last_name'];
+			return $_POST['last_name'];
 		}else{
-			//Throw non-alphabetic name error
+			header("Location:index.php?err=ln&type=nonalpha");
+			die();
 		}
 	}else{
-		//Throw missing error
-	}
-	
+		header("Location:index.php?err=ln&type=missing");
+		die();
+	}	
+}
+
+function validate_email(){
 	if(isset($_POST['email'])){
-		$email = $_POST['email'];
+		return $_POST['email'];
 	}else{
-		//Throw missing error
-	}
-	
+		header("Location:index.php?err=email&type=missing");
+		die();
+	}	
+}
+
+function validate_password(){
 	if(isset($_POST['password']) && isset($_POST['password_retype'])){
 		if($_POST['password'] == $_POST['password_retype']){
 			if(strlen($_POST['password']) >= 6){
-				$pass = password_hash($_POST['password'], PASSWORD_DEFAULT);
+				return password_hash($_POST['password'], PASSWORD_DEFAULT);
 			}else{
-				//Throw password too short error
+				header("Location:index.php?err=pass&type=short");
+				die();
 			}
 		}else{
-			//Throw passwords don't match error
+			header("Location:index.php?err=pass&type=match");
+			die();
 		}
 	}else{
-		//Throw missing error
-	}
+		header("Location:index.php?err=pass&type=missing");
+		die();
+	}	
+}
+	
+function email_exists($db, $e){
+	$val_email = $db->prepare('SELECT COUNT(*) FROM users WHERE email = :email');
+	$val_email->bindParam(":email", $e);
+	$val_email->execute();
+	return $val_email->fetchColumn();
+}
+	
+function create_account($db, $fn, $ln, $e, $p){
+	$act = $db->prepare('INSERT INTO users (first_name, last_name, email, pass, registration_date) VALUES (:first_name, :last_name, :email, :pass, now())');
+	$act->bindParam(":first_name", $fn);
+	$act->bindParam(":last_name", $ln);
+	$act->bindParam(":email", $e);
+	$act->bindParam(":pass", $p);
+	return $act->execute();
+}
+	
+try {	
+	$first_name = validate_first_name();
+	$last_name = validate_last_name();
+	$email = validate_email();
+	$pass = validate_password();
 			
 	
 	$conn = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME", $DB_USERNAME, $DB_PASSWORD);
 	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	
-	$validate_email = $conn->prepare('SELECT COUNT(*) FROM users WHERE email = :email');
-	$validate_email->bindParam(":email", $email);
-	$validate_email->execute();
-	$validate_email_result = $validate_email->fetchColumn();
 	
-	if($validate_email_result > 0){
-		//Throw account already exists error
+	if(email_exists($conn, $email) > 0){
+		header("Location:index.php?err=email&type=exists");
+		die();
 	}else{
-		$create_account = $conn->prepare('INSERT INTO users (first_name, last_name, email, pass, registration_date) VALUES (:first_name, :last_name, :email, :pass, now())');
-		$create_account->bindParam(":first_name", $first_name);
-		$create_account->bindParam(":last_name", $last_name);
-		$create_account->bindParam(":email", $email);
-		$create_account->bindParam(":pass", $pass);
-		$create_account->execute();
-		
-		if($create_account){
-			$account_id = $conn->lastInsertId('user_id');
-			
+		if(create_account($conn, $first_name, $last_name, $email, $pass)){
+			$account_id = $conn->lastInsertId('user-id');
 			
 		}else{
-			//Throw error on insert
+			header("Location:index.php?err=db");
+			die();
 		}
 	}
 }
 catch(PDOException $e){
-	//Throw database connection error	
+	header("Location:index.php?err=db");	
 }
 
 ?>
