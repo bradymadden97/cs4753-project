cc4c11cff25a7acb6ac1802cee95d4cb7113e01d
Brady Madden <bradymadden97@gmail.com>
2017-09-25 10:37:26 -0400

    starting client val

--- register/register.js
+++ register/register.js
@@ -1,3 +1,22 @@
+//Remove error messages when typing begins
+$("#inputFirstName").keyup(function(){
+	$("#firstnamefeedback").hide();
+});
+$("#inputLastName").keyup(function(){
+	$("#lastnamefeedback").hide();
+});
+$("#inputEmail").keyup(function(){
+	$("#emailfeedback").hide();
+});
+$("#inputPassword").keyup(function(){
+	$("#passwordfeedback").hide();
+});
+$("#inputPasswordCheck").keyup(function(){
+	$("#passwordcheckfeedback").hide();
+});
+
+
+//Validating password and retype password
 $("#inputPassword").keyup(function(){
 	if($("#inputPassword").val() !== $("#inputPasswordCheck").val()){
 		$("#signupbtn").prop('disabled', true);
@@ -27,6 +46,7 @@ $("#inputPasswordCheck").blur(function(){
 	}
 });
 
+//Onsubmit validation
 window.addEventListener("load", function(){
 	var form = document.getElementById("signupform");
 	form.addEventListener("submit", function(event){
