892c1144732316153678117caa1817c736958d6c
Brady Madden <bradymadden97@gmail.com>
2017-10-02 13:17:09 -0400

    client val

--- register/register.js
+++ register/register.js
@@ -1,20 +1,44 @@
 //Remove error messages when typing begins
-$("#inputFirstName").keyup(function(){
-	$("#firstnamefeedback").hide();
+$("#inputFirstName").blur(function(){
+	if($("#inputFirstName").val().trim() != ""){
+		$("#firstnamefeedback").hide();
+	}else{
+		$("#firstnamefeedback").text("First name cannot be empty");
+		$("#firstnamefeedback").show();
+	}
 });
-$("#inputLastName").keyup(function(){
-	$("#lastnamefeedback").hide();
+$("#inputLastName").blur(function(){
+	if($("#inputLastName").val().trim() != ""){
+		$("#lastnamefeedback").hide();
+	}else{
+		$("#lastnamefeedback").text("Last name cannot be empty");
+		$("#lastnamefeedback").show();
+	}
 });
-$("#inputEmail").keyup(function(){
-	$("#emailfeedback").hide();
+$("#inputEmail").blur(function(){
+	if($("#inputEmail").val().trim() != ""){
+		$("#emailfeedback").hide();
+	}else{
+		$("#emailfeedback").text("Email cannot be empty");
+		$("#emailfeedback").show();
+	}
 });
-$("#inputPassword").keyup(function(){
-	$("#passwordfeedback").hide();
+$("#inputPassword").blur(function(){
+	if($("#inputPassword").val().trim() != ""){
+		$("#passwordfeedback").hide();
+	}else{
+		$("#passwordfeedback").text("Password cannot be empty");
+		$("#passwordfeedback").show();
+	}
 });
-$("#inputPasswordCheck").keyup(function(){
-	$("#passwordcheckfeedback").hide();
+$("#inputPasswordCheck").blur(function(){
+	if($("#inputPasswordCheck").val().trim() != ""){
+		$("#passwordcheckfeedback").hide();
+	}else{
+		$("#passwordcheckfeedback").text("Password cannot be empty");
+		$("#passwordcheckfeedback").show();
+	}
 });
-
 
 //Validating password and retype password
 $("#inputPassword").keyup(function(){
