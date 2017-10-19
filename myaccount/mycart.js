84b08ca510f9d7ecbaac6add867a825cdad367a4
Brady Madden <bradymadden97@gmail.com>
2017-10-19 16:30:53 -0400

    Update mycart.js

--- myaccount/mycart.js
+++ myaccount/mycart.js
@@ -5,9 +5,7 @@ $(".cartremoveX").click(function(){
 		item_id: t.attr('data-remove-id'),
 	},
 	function(d, s){
-		console.log(d);
 		if(d == 1){
-			console.log("hello world");
 			t.parent().parent().css('display', 'none');
 		}
 	});
