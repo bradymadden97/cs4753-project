b24c870aa45cf7aa6202c08efce1e3bab2661707
Robert Hails <rlh2ph@virginia.edu>
2017-10-20 15:43:28 -0400

    added functionality to verify page

--- register/verify.php
+++ register/verify.php
@@ -1,9 +1,49 @@
+<?php
+
+session_start();
+
+//Includes database connection variables
+require_once("../config/config.php");
+include('../register/register.php');
+
 function getVerification($email,$code){
 	$query_check = "UPDATE `email_var` SET `status` = '1' where `email` = '$email' && `code` = '$code'";
 	if (mysql_query($query_check)) {
 		return true;
 	}
-	else{
+  else{
 		return false;
 	}
 }
+
+try {
+	//Use either PDO or MySQLi to prevent against SQL injection attacks
+	$conn = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME", $DB_USERNAME, $DB_PASSWORD);
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  if($_GET['email'] && $_GET['code']){
+    $email = $_GET['email'];
+    $code = $_GET['code'];
+
+    $check_status = getVerification($email, $code);
+
+    if($check_status){
+      header("Location:/register/welcome")
+    }
+    else{
+      header("Location:index.php?err=db")
+    }
+
+  }
+  
+}
+catch(PDOException $e){
+	header("Location:index.php?err=db");
+}
+
+
+
+
+
+
+?>
